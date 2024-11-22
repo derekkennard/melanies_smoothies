89a216d5ecf47b0ee0b92bd180edf65a69b4f917
Derek Kennard <71831448+derekkennard@users.noreply.github.com>
2024-11-21 19:31:25 -0700

    Update streamlit_app.py
    added new request

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,11 @@ if ingredients_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
-
+    # New section to display smoothiefroot nutrition information
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
+    
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
